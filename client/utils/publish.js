f2f371184853f93d5339215de587c430c9e450a7
Christian Broms <cb@christianbroms.com>
2022-02-12 22:54:05 -0800

    publish latest changes to cdn with actions

--- client/utils/publish.js
+++ client/utils/publish.js
@@ -13,10 +13,6 @@ const publish = async () => {
     `/link-previews/${process.env.TAG}/hyperfov-link-previews.js`
   );
 
-  const params = new URLSearchParams();
-  params.append("value", file);
-  params.append("metadata", {});
-
   const res = await fetch(
     `https://api.cloudflare.com/client/v4/accounts/${process.env.CF_ACCOUNT_ID}/storage/kv/namespaces/${process.env.CF_KV_NAMESPACE}/values/${loc}`,
     {
@@ -25,7 +21,7 @@ const publish = async () => {
         "X-Auth-Email": process.env.CF_EMAIL,
         "X-Auth-Key": process.env.CF_API_TOKEN,
       },
-      body: params,
+      body: file,
     }
   );
   return res.ok;
