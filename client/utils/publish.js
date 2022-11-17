5b88e5c9faf5d7a4905181256f4f5cc84a898cd2
Christian Broms <cb@christianbroms.com>
2022-11-16 22:18:58 -0800

    add css file to CDN deploy step

--- client/utils/publish.js
+++ client/utils/publish.js
@@ -5,26 +5,47 @@ const path = require("path");
 // publish the package to the hyperfov CDN
 // this is run from the github action
 const publish = async () => {
-  const file = fs
+  const jsFile = fs
     .readFileSync(path.join(__dirname, "../dist/hyperfov-link-previews.js"))
     .toString();
 
-  const loc = encodeURIComponent(
+  const loc1 = encodeURIComponent(
     `/link-previews/${process.env.TAG}/hyperfov-link-previews.js`
   );
 
-  const res = await fetch(
-    `https://api.cloudflare.com/client/v4/accounts/${process.env.CF_ACCOUNT_ID}/storage/kv/namespaces/${process.env.CF_KV_NAMESPACE}/values/${loc}`,
+  const res1 = await fetch(
+    `https://api.cloudflare.com/client/v4/accounts/${process.env.CF_ACCOUNT_ID}/storage/kv/namespaces/${process.env.CF_KV_NAMESPACE}/values/${loc1}`,
     {
       method: "PUT",
       headers: {
         "X-Auth-Email": process.env.CF_EMAIL,
         "X-Auth-Key": process.env.CF_API_TOKEN,
       },
-      body: file,
+      body: jsFile,
     }
   );
-  return res.ok;
+
+  const cssFile = fs
+    .readFileSync(path.join(__dirname, "../dist/hyperfov-link-previews.css"))
+    .toString();
+
+  const loc2 = encodeURIComponent(
+    `/link-previews/${process.env.TAG}/hyperfov-link-previews.css`
+  );
+
+  const res2 = await fetch(
+    `https://api.cloudflare.com/client/v4/accounts/${process.env.CF_ACCOUNT_ID}/storage/kv/namespaces/${process.env.CF_KV_NAMESPACE}/values/${loc2}`,
+    {
+      method: "PUT",
+      headers: {
+        "X-Auth-Email": process.env.CF_EMAIL,
+        "X-Auth-Key": process.env.CF_API_TOKEN,
+      },
+      body: cssFile,
+    }
+  );
+
+  return res1.ok && res2.ok;
 };
 
 publish();
